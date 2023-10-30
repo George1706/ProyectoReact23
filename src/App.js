88676dd5ccbf1a087e24e589b87a0cd93b2d55aa
George1706 <jorjpineda@soy.sena.edu.co>
2023-10-29 22:11:12 -0500

    logn register y un desboard aun sin nada y logout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React, {Fragment} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from './paginas/Auth/login';
+const express = require('express');
+const { engine } = require('express-handlebars');
+const myconnection = require('express-myconnection');
+const mysql = require('mysql');
+const session = require ('express-session');
+const bodyParser = require('body-parser');
 
-function App() {
-  return (
-    <Fragment>
-      <Router>
-        <Routes>
-          <Route path='/' exact element = {<Login/>}>
-            
-          </Route>
-        </Routes>
-      </Router>
-    </Fragment>
-  );
-}
+const loginRoutes = require('./routes/login');
 
-export default App;
+const app = express();
+app.set('port', 4000);
+
+app.set('views', __dirname + '/views');
+app.engine('.hbs', engine({
+    extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({
+    entended: true
+}));
+app.use(bodyParser.json());
+
+
+app.use(myconnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'OMEGA_PET_SHOP'
+}))
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.listen(app.get('port'), () => {
+    console.log('Listening on port', app.get('port'));
+});
+
+app.use('/', loginRoutes);
+
+app.get('/', (req, res) => {
+    if(req.session.loggedin == true){
+
+        res.render('home', {name: req.session.name});
+
+    }else {
+        res.redirect('/login');
+    }
+});
